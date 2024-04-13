2cb1bddfd931d3032f128cb8cc743a8ab1ca578c
unknown <rodrigow_@hotmail.com.br>
2024-04-13 16:30:28 -0300

    First commit

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import dynamic from "next/dynamic";
+import { useMemo } from "react";
+
+export default function Home() {
+  const Map = useMemo(() => dynamic(
+    () => import('@/app/components/Map'),
+    { 
+      loading: () => <p>A map is loading</p>,
+      ssr: false
+    }
+  ), [])
+
+  return <main className="h-[100vh]">
+    <Map/>
+  </main>
+}
